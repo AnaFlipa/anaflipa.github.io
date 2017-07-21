d0ad1c251ebf3f5c98eb560b4c47d465d8399696
Ana Alves <anaflipasa3@gmail.com>
2017-07-21 23:41:47 +0100

    about

--- js/main.js
+++ js/main.js
@@ -1,41 +1,47 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t, n, r) { function s(o, u) { if (!n[o]) { if (!t[o]) { var a = typeof require == "function" && require; if (!u && a) return a(o, !0); if (i) return i(o, !0); throw new Error("Cannot find module '" + o + "'") } var f = n[o] = { exports: {} }; t[o][0].call(f.exports, function (e) { var n = t[o][1][e]; return s(n ? n : e) }, f, f.exports, e, t, n, r) } return n[o].exports } var i = typeof require == "function" && require; for (var o = 0; o < r.length; o++)s(r[o]); return s })({
+    1: [function (require, module, exports) {
 
-},{}]},{},[1])
+    }, {}]
+}, {}, [1])
 
 
 
 
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover(); 
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
 });
 
 
 
 
 
-$(document).ready(function(){
-
-    $(".filter-button").click(function(){
+$(document).ready(function () {
+    $(".filter-button").click(function () {
         var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
+
+        if (value == "all") {
             //$('.filter').removeClass('hidden');
             $('.filter').show('1000');
         }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
+        else {
+            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
         }
     });
-    
-    if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
 
+
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+    var username = "anaflipasa3";
+    var hostname = "gmail.com";
+
+    $("#email-location").html(username + "@" + hostname);
 });
+
+
+
+
