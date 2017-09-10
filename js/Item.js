0ae1164fa1f97231c22be80ac0801dbce16c7a83
Ana Alves <anaflipasa3@gmail.com>
2017-09-10 20:55:00 +0100

    game jam

---
+++ js/Item.js
@@ -0,0 +1,20 @@
+/*<div class="col-sm-4 col-md-3 filter ux/ui">
+              <div class="box">
+                <div class="containerportfolio"> <img src="img/portfolio/coopar.jpg" alt="" class="img-responsive">
+                  <div class="overlayportfolio">
+                    <div class="textportfolio">
+                      <center><a href="https://www.behance.net/gallery/53293979/Coop-AR-2017">
+                        <b>Coop AR</b></a><br> UX/UI Design <br><br>
+                        <p class="dateportfolio">2017</p>
+                      </center>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>*/
+
+function Item(filter, image, link, name, filterDescription, date)
+{
+    this.filter = filter;
+}
+
